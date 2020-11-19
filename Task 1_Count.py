7dc2ade9a80abeb2ecef3b1cbd34444a6f10fddf
Quinn <Keith.Quinn@bsci.com>
2020-11-19 08:31:13 +0000

    Folder structure clean up to include all tasks in one Jupyter Notebook

--- Task 1_Count.py
+++
@@ -1,9 +0,0 @@
-mylist = [1, 2, 5, 4, 1, 1, 3, 2, 1, 1, 1, 'e', 'e']
-
-mydict = {}
-
-for i in range (len(mylist)):
-    mydict[mylist[i]] = mylist.count(mylist[i])
-    print(mydict)
-
-print(mydict)
